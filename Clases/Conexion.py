ff45015e6f8f7984d3d7ac96a290d917b66331b6
neoengelus <106979414+neoengelus@users.noreply.github.com>
2022-11-17 12:48:02 -0300

    Secon Commit
    Added Connection Class

---
+++ Clases/Conexion.py
@@ -0,0 +1,27 @@
+'''
+Created on 17 nov. 2022
+
+@author: engelus
+'''
+import sqlite3 as sql
+
+class Conexion_BD():
+
+  def __init__(self,bd):#bd es el nombre de la base de datos
+    self.conexion = sql.connect(bd)
+    self.cursor = self.conexion.cursor()
+
+  def consulta(self, consulta):
+    self.cursor.execute(consulta)
+    
+  def many(self,consulta):
+    self.cursor.executemany(consulta)
+  
+  def script(self,script):
+    self.cursor.executescript(script)
+
+  def commit(self):
+    self.conexion.commit()
+
+  def cerrar(self):
+    self.conexion.close()
