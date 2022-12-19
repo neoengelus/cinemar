63fee90f1d574ffcbc5545d269a051e547b5b4dc
Ariel Chocobar <arielteacher4@gmail.com>
2022-12-18 22:50:57 -0300

    crudpeli al 95%

--- Vistas/vCRUD peli.py
+++ Vistas/vCRUD peli.py
@@ -15,19 +15,26 @@ class PantallaModi(QWidget):
         loadUi(".\Vistas\CRUDPeli.ui",self)
         self.id=0
         self.cargaTablaPeli()
-        self.btnConfirmar.clicked.connect(self.revisarCampos)
+        
         self.btnEli.clicked.connect(self.eliminarPeli)
         self.btnNuevo.clicked.connect(self.limpiaCampos)
         self.btnGrabaNueva.clicked.connect(self.grabaPeli)
+        self.btnConfirmar.clicked.connect(self.modiPeli)
         self.tableListUsu.clicked.connect(lambda : self.celdaClick())
+        self.btnSalir.clicked.connect(self.salepantalla)
         #print (self.id)
-        list_clasi = ["PM13", "PM16", "PM18"]
+        list_clasi = ["PM13", "PM16", "PM18","ATP"]
         self.comboClasi.addItems(list_clasi)
-        self.tableListUsu.setColumnWidth(1, 400)
+        self.tableListUsu.setColumnWidth(1, 300)
         self.tableListUsu.setSelectionBehavior(QTableView.SelectRows)
         self.btnGrabaNueva.setVisible(False)
         
-    
+    def salepantalla(self):
+        try:
+            widget.close()
+        except:
+            print("Exiting")
+        
     def celdaClick(self):
         lista = self.tableListUsu.currentRow()
         self.id = self.tableListUsu.item(lista, 0).text()
@@ -35,6 +42,16 @@ class PantallaModi(QWidget):
         self.boxTitulo.setText (self.tableListUsu.item(lista, 1).text())
         self.boxDuracion.setText (self.tableListUsu.item(lista, 3).text())
         self.boxDirector.setText (self.tableListUsu.item(lista, 2).text())
+        if self.tableListUsu.item(lista, 4).text()=="PM13":
+            self.comboClasi.setCurrentIndex(0)
+        elif self.tableListUsu.item(lista, 4).text()=="PM16":
+            self.comboClasi.setCurrentIndex(1)
+        elif self.tableListUsu.item(lista, 4).text()=="PM18":
+            self.comboClasi.setCurrentIndex(2)
+        elif self.tableListUsu.item(lista, 4).text()=="ATP":
+            self.comboClasi.setCurrentIndex(3)
+        else:
+            self.comboClasi.setCurrentIndex(3)           
         return 
     def limpiaCampos(self):
         self.btnGrabaNueva.setVisible(True)
@@ -60,7 +77,27 @@ class PantallaModi(QWidget):
             self.btnGrabaNueva.setVisible(False)
             
             self.cargaTablaPeli()
-            
+    def modiPeli(self):
+        idpeli=self.id
+        titulo=self.boxTitulo.text ()
+        duracion=self.boxDuracion.text ()
+        clasi=self.comboClasi.currentText()
+        director=self.boxDirector.text ()
+        if duracion.isdigit()!=True:
+            QMessageBox.critical(self, "Error","Duracion debe ser un Numero")
+            return
+        if titulo=="" or duracion=="" or director=="" :
+            QMessageBox.critical(self, "Error","No pueden haber campos vacios")
+            return
+        else:
+            conn = sqlite3.connect(BD)
+            cur = conn.cursor()
+            query =f"update pelicula set titulo='{titulo}', duracion='{duracion}',clasificacion='{clasi}', director='{director}' where id_pelicula='{idpeli}'"
+            cur.execute(query)
+            conn.commit()
+            QMessageBox.critical(self, "Exito","Se ha modificado la pelicula Exitosamente")
+            self.cargaTablaPeli()  
+                  
     def eliminarPeli(self):
         Idpeli = self.boxIdpeli.text()
         resp = QMessageBox.question(self, 'Advertencia', "Desea borrar la Pelicula?", QMessageBox.Yes | QMessageBox.Cancel)
@@ -80,27 +117,6 @@ class PantallaModi(QWidget):
         
     
         
-    def revisarCampos(self):
-        Idpeli=self.boxIdpeli.text ()
-        tit=self.boxNombre.text ()
-        dire=self.boxDirector.text()
-        clasi=self.boxcomboClasi.currentText()
-        dur=self.boxDuracion.text()
-       
-        if tit=="" or dire=="" or dur=="" :
-            QMessageBox.critical(self, "Error","No pueden haber campos vacios")
-        
-        else:
-            
-            conn = sqlite3.connect(BD)
-            cur = conn.cursor()
-            query =f"UPDATE pelicula SET titulo = '{tit}', duracion = '{dur}',clasificacion='{clasi}', director='{dire}' WHERE id_pelicula ='{Idpeli}'"
-            
-            cur.execute(query)
-            conn.commit()
-            conn.close()
-            self.llenarboxes()
-            QMessageBox.critical(self, "Exito","Se han modificado los Datos Exitosamente")
             
     def cargaTablaPeli(self):
         conn = sqlite3.connect(BD)
@@ -113,7 +129,7 @@ class PantallaModi(QWidget):
         conn.close
         tablerow = 0
         for fila in setPelis:
-            self.tableListUsu.setItem(tablerow, 0, QtWidgets.QTableWidgetItem(str(fila[1])))
+            self.tableListUsu.setItem(tablerow, 0, QtWidgets.QTableWidgetItem(str(fila[0])))
             self.tableListUsu.setItem(tablerow, 1, QtWidgets.QTableWidgetItem(fila[3]))
             self.tableListUsu.setItem(tablerow, 2, QtWidgets.QTableWidgetItem(fila[2]))           
             self.tableListUsu.setItem(tablerow, 3, QtWidgets.QTableWidgetItem(fila[1]))
